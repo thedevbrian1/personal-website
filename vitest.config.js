679ce80543d3659b77ae9ff219cba06c30193e92
thedevbrian1 <brayomwas95@gmail.com>
2022-09-06 12:31:10 +0300

    Used es6 import

--- vitest.config.js
+++ vitest.config.js
@@ -1,11 +1,14 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-const react = require("@vitejs/plugin-react");
+// const react = require("@vitejs/plugin-react");
+import react from "@vitejs/plugin-react"
 
-const { defineConfig } = require("vite");
+// const { defineConfig } = require("vite");
+import { defineConfig } from "vite";
 
-const tsconfigPaths = require("vite-tsconfig-paths");
+// const tsconfigPaths = require("vite-tsconfig-paths");
+import { tsconfigPaths } from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
